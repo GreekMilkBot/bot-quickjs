7d906d260b83eff73e7e58e3103f5a7f17bacb84
dragon <ExplodingFKL@gmail.com>
2025-06-18 17:33:16 +0800

    testing

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/GreekMilkBot/bot-quickjs
 go 1.24.3
 
 require (
-	github.com/GreekMilkBot/GreekMilkBot v0.0.0-20250617072306-6a18b32a361e
+	github.com/GreekMilkBot/GreekMilkBot v0.0.0-20250618022342-1cc44d90cd9d
 	github.com/buke/quickjs-go v0.5.7
 	github.com/buke/quickjs-go-polyfill v0.0.9
 	github.com/goccy/go-yaml v1.18.0
--- task.go
+++ task.go
@@ -3,11 +3,14 @@ package main
 type Task struct {
 	Title        string                       `yaml:"title"`
 	Describe     string                       `yaml:"describe"`
+	Parameters   map[string]interface{}       `yaml:"parameters"`
 	Script       string                       `yaml:"script,flow"`
 	At           bool                         `yaml:"at"`
 	GlobalConfig map[string]string            `yaml:"global_config"`
 	ScopeConfig  map[string]map[string]string `yaml:"scope_config"`
 
 	Bytecode []byte `yaml:"-"` // js 可用的 bytecode
+}
 
+type TaskParam struct {
 }
